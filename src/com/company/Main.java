98690d1ce481e95332c6d187e438e8900ec7da26
RGiffari <11R02Giffari@users.noreply.github.com>
2020-10-20 23:56:51 +0700

    initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,30 @@
+package com.company;
+
+/**
+ *
+ * @author
+ * NAMA         : Raihan Giffari
+ * KELAS        : PBO12
+ * NIM          : 10119008
+ * Deskripsi Program    : Program ini berisi tentang variabel integer
+ *
+ */
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        byte b;
+        short s;
+        int i;
+        long l;
+        b = 120;
+        s = 32767;
+        i = 1_000_000_000;
+        l = 90000000000L;
+        System.out.println("byte : " + b);
+        System.out.println("short : " + s);
+        System.out.println("int : " + i);
+        System.out.println("long : " + l);
+    }
+}
